4cb4acac2e1eb43ca9dd8086994cc1fa670676d4
lanajahshan1 <137297023+lanajahshan1@users.noreply.github.com>
2023-06-26 10:45:17 +0300

    Add files via upload

--- run_on_VM.ps1
+++ run_on_VM.ps1
@@ -1,3 +1,6 @@
 Get-AzVM -ResourceGroupName "Candidates_Homework_Lana" -Name "simple-vm"
 
-Invoke-AzVMRunCommand -ResourceGroupName 'Candidates_Homework_Lana' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'working_with_blobs.ps1' 
+$result = Invoke-AzVMRunCommand -ResourceGroupName 'Candidates_Homework_Lana' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'working_with_blobs.ps1' 
+
+$output = $result.Value[0].Message
+Write-Output "Script Output: $output"
--- working_with_blobs.ps1
+++ working_with_blobs.ps1
@@ -2,10 +2,6 @@ if (-not (Get-Module -ListAvailable Az.Storage)) {
   Install-Module -Name Az.Storage -Force
 }
 
-Connect-AzAccount
-
-Set-AzContext -Subscription 75a700f9-db14-47c2-b97c-4481c0611f0b
-
 $sourceStorageAccountName = "lanastorageaccount1"
 $destinationStorageAccount = "lanastorageaccount2"
 $sourceContainerName = "sourceblobcontainer"
@@ -26,9 +22,6 @@ for ($i = 1; $i -le 100; $i++) {
     New-Item -Path . -Name $blobName -ItemType "file" -Value "This is a blob." -Force
     Set-AzStorageBlobContent -Container $sourceContainerName -File $blobName -Blob $blobName -Context $sourceContext -Force
     Remove-Item -Path "file$i.txt" -Force
+
     Start-AzStorageBlobCopy -SrcContainer $sourceContainerName -SrcBlob $blobName -DestContainer $destinationContainerName -DestBlob $blobName -Context $sourceContext -DestContext $destinationContext
-
 }
-
-
-Disconnect-AzAccount
